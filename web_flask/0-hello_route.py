930854cb94a97322db523a7e6ae1a43170ee42a2
chessboy71 <bentegrimohamed@gmail.com>
2024-04-21 18:37:47 +0000

    s

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-from flask import Flask
 
+"""
+start Flask application
+"""
+
+from flask import Flask
 app = Flask(__name__)
 
-app.url_map.strict_slashes = False
 
-@app.route("/")
-def helloRoute():
-	return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
-if __name__ == "__main__":
-	app.run(debug = True, host = "0.0.0.0", port = 5000)
+if __name__ == '__main__':
+    app.run(debug=true, host='0.0.0.0', port='5000')
