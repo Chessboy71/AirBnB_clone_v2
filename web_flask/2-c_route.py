3896f9510a2c2c9fe9c1474d44da06e6fa52d4b8
chessboy71 <bentegrimohamed@gmail.com>
2024-04-21 18:55:42 +0000

    S

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+start Flask application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbhb():
+    """returns HBNB"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cRoute(text):
+    """returns c with the text after"""
+    text = text.replace('_',' ')
+    return 'C ' + text
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
